1a634c0c1b89f4f06900c88dd206ed11bca4d97d
Игорь Фетов <igorfetov1997@gmail.com>
2022-04-04 11:42:00 +0300

    Добавьте файлы проекта.

---
+++ ArrayMax/Source.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <utility>
+
+void bubbleSort(int length, int array[]) {
+	int min = 0;
+	int iter = 0;
+	for (int iteration = 0; iteration < length - 1; ++iteration) {
+		min = array[0];
+		iter = iteration + 1;
+		for (int index = 1; index < length; ++index) {
+			if (array[index] < array[index - 1]) {
+				min = array[index];
+				std::swap(array[index], array[index - 1]);
+			}
+		}
+		if (min == array[0])
+			break;
+	}
+	std::cout << "Iteration is: " << iter << std::endl;
+	
+	for (int index = 0; index < length; ++index)
+		std::cout << array[index] << ' ';
+}
+
+
+int main() {
+	const int length = 9;
+	int array[length]{ 1, 5, 6, 4, 9, 8, 2, 7, 3 };
+	
+	return 0;
+}
